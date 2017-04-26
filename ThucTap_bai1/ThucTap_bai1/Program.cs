0b2c336247bb5c29e791d98116ecd20f6c515bb9
Chien Nguyen <nguyenchien27081996@gmail.com>
2017-04-26 22:22:30 +0700

    thêm phần hướng dẫn sử dụng

--- ThucTap_bai1/ThucTap_bai1/Program.cs
+++ ThucTap_bai1/ThucTap_bai1/Program.cs
@@ -16,7 +16,7 @@ namespace ThucTap_bai1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
